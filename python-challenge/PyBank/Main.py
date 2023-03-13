0462f155fbabe1d36571bce3e46466b1c2d6450e
Mangocircle <125223177+Mangocircle@users.noreply.github.com>
2023-03-13 02:07:54 -0600

    Add files via upload

---
+++ python-challenge/PyBank/Main.py
@@ -0,0 +1,72 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+import os
+import csv
+
+
+# In[3]:
+
+
+with open('Resources/budget_data.csv',encoding='utf') as csvfile:
+    csvreader=csv.reader(csvfile,delimiter=',')
+    next(csvreader)
+    rowcount=0
+    ploss=0
+    values=[]
+    cbv=[]
+    Data=[]
+    for row in csvreader:
+        Data.append(row)
+        rowcount+=1
+        ploss+=int(row[1])
+        values.append(int(row[1]))
+    for i in range(1,len(values)):
+        cbv.append(values[i]-values[(i-1)])
+    def avg(numbers):
+        average=(sum(numbers))/len(numbers)
+        return average
+    cbva=round(avg(cbv),2)
+    maxi=max(cbv)
+    mini=min(cbv)
+
+    for x in range(len(cbv)):
+        if mini == cbv[x]:
+                     #   V skipped header row and added one to skip initial empty value
+            Gdec=(Data[x+1][0]+" "+"$"+str(cbv[x]))
+        if maxi == cbv[x]:
+                     #   V skipped header row and added one to skip initial empty value
+            Ginc=(Data[x+1][0]+" "+"$"+str(cbv[x]))
+
+
+# In[4]:
+
+
+print("Total Months: "+str(rowcount))
+print("Total: "+"$"+str(ploss))
+print("Average Change: "+"$"+str(cbva))
+print("Greatest Increase in Profits: "+Ginc)
+print("Greatest Decrease in Profits: "+Gdec)
+
+
+# In[5]:
+
+
+f=open("Results.txt","w")
+f.write("Financial Analysis"+'\n'+'\n'+"----------------------------"+'\n'+'\n')
+f.write("Total Months: "+str(rowcount)+'\n')
+f.write("Total: "+"$"+str(ploss)+'\n')
+f.write("Average Change: "+"$"+str(cbva)+'\n')
+f.write("Greatest Increase in Profits: "+Ginc+'\n')
+f.write("Greatest Increase in Profits: "+Gdec+'\n')
+f.close()
+
+
+# In[ ]:
+
+
+
+
---
+++ python-challenge/README.md
@@ -0,0 +1,2 @@
+# python-challenge
+Files for the module 3 challenge otherwise known as python challenge
